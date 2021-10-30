8310d71804d4ab23d0aed3148b3df9e86ab106ee
artem.zatula <artem.zatula@clickatell.com>
2021-10-30 10:09:58 +0300

    [MongoDb]: setup comunication between containers

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose
+  .connect("mongodb://artem:pass@mongo:27017?authSource=admin")
+  .then(() => console.log("succesfully connected to DB"))
+  .catch(e => console.log("Error", e))
+
 app.get('/', (req, res) => {
   res.send('<h2>Hi there!!!</h2>');
 });
