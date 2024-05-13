b374a9ff0fcc1af4b9df8cc145504c09cd331a42
Artem Zatula <artemzatula@gmail.com>
2024-05-13 23:22:08 +0300

    feat: add possibilty to find ws by userId

--- controllers/workspace.controller.js
+++ controllers/workspace.controller.js
@@ -2,13 +2,12 @@ import Workspace from '../models/workspace.model.js';
 
 export async function getAllWorkspaces(req, res) {
   try {
-    const workspaces = await Workspace.find();
+    const userId = req.session.user._id
+    const workspaces = await Workspace.find({users:  { $elemMatch: { userId } }})
     res.status(200).json({
       status: 'success',
       results: workspaces.length,
-      data: {
-        workspaces
-      }
+      data: { workspaces }
     })
   } catch(e) {
     res.status(400).json({
@@ -18,16 +17,20 @@ export async function getAllWorkspaces(req, res) {
 }
 
 export async function createWorkspace(req, res) {
-  console.log(req)
   try {
-    const workspace = await Workspace.create(req.body);
-    // res.status(200).json({
-    //   status: 'success',
-    //   results: workspaces.length,
-    //   data: {
-    //     workspaces
-    //   }
-    // })
+    const ws = {
+      name: req.body.name || 'Undefined',
+      users: [{
+        userId: req.session.user._id,
+        role: 'Owner'
+      }]
+    }
+    const workspace = await Workspace.create(ws);
+    res.status(200).json({
+      status: 'success',
+      results: workspaces.length,
+      data: { workspaces }
+    })
   } catch(e) {
     res.status(400).json({
       status: 'failure'
